53520c59b8bcf56e39fc4e1208ec2d545dfef55e
Lewis Williams <60359937+LewisW99@users.noreply.github.com>
2025-12-15 09:34:12 +0000

    API Push

--- GameCatalogue.API/Program.cs
+++ GameCatalogue.API/Program.cs
@@ -1,10 +1,21 @@
+using GameCatalogue.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<GameCatalogueDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("GameCatalogueDb")));
+
 
 var app = builder.Build();
 
